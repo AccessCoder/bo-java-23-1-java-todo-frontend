d0a5d6dd24cbaf04760c5f7778bfcc50a54b2fe1
Kreyno93 <michael.wagener@neuefische.de>
2023-05-08 13:21:27 +0200

    adding comment for alternative

--- backend/src/test/java/de/neuefische/backend/controller/todoControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/todoControllerTest.java
@@ -37,7 +37,7 @@ class todoControllerTest {
 
         //WHEN & THEN
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/todo"))
+                        MockMvcRequestBuilders.get("/api/todo"))
                 .andExpect(status().is(200))
                 .andExpect(content().json("[]"));
 
@@ -54,9 +54,9 @@ class todoControllerTest {
                 """;
 
         MvcResult response = mockMvc.perform(
-                MockMvcRequestBuilders.post("/api/todo")
-                        .contentType("application/json")
-                        .content(responseBody))
+                        MockMvcRequestBuilders.post("/api/todo")
+                                .contentType("application/json")
+                                .content(responseBody))
                 .andReturn();
 
         String content = response.getResponse().getContentAsString();
@@ -65,7 +65,7 @@ class todoControllerTest {
         //WHEN & THEN
 
         mockMvc.perform(
-                MockMvcRequestBuilders.get("/api/todo/"+todo.getId()))
+                        MockMvcRequestBuilders.get("/api/todo/" + todo.getId()))
                 .andExpect(status().is(200))
                 .andExpect(content().json(responseBody))
                 .andExpect(jsonPath("$.id").value(todo.getId()));
@@ -77,14 +77,16 @@ class todoControllerTest {
 
         //WHEN & THEN
 
-            mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/imInvalid"))
-                    .andExpect(status().is(404))
-                    .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
-                    .andExpect(result -> {
-                        ResponseStatusException exception = (ResponseStatusException) result.getResolvedException();
-                        assert exception != null;
-                        assertEquals("404 NOT_FOUND \"No Todo found with ID:imInvalid\"", exception.getMessage());
-                    });
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/imInvalid"))
+                .andExpect(status().is(404))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ResponseStatusException))
+                .andExpect(result -> {
+                    ResponseStatusException exception = (ResponseStatusException) result.getResolvedException();
+                    assert exception != null;
+                    assertEquals("404 NOT_FOUND \"No Todo found with ID:imInvalid\"", exception.getMessage());
+                });
+        // besser wäre es für den User, wenn eine Eindeutige, vllt auch einzeilige Fehlermeldung zurückgegeben wird
+        // nach dieser können wir auch besser testen statt, das Object so zu manipulieren bis wir an die Fehlermeldung kommen
 
     }
 
@@ -99,14 +101,13 @@ class todoControllerTest {
                 """;
         //WHEN & THEN
         mockMvc.perform(
-                MockMvcRequestBuilders.post("/api/todo")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
+                        MockMvcRequestBuilders.post("/api/todo")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody))
                 .andExpect(status().is(200))
                 .andExpect(content().json(requestBody))
                 .andExpect(jsonPath("$.id").isNotEmpty());
     }
-
 
 
     @Test
@@ -130,9 +131,9 @@ class todoControllerTest {
 
         //WHEN & THEN
         mockMvc.perform(
-                MockMvcRequestBuilders.put("/api/todo/"+todo.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(responseBody))
+                        MockMvcRequestBuilders.put("/api/todo/" + todo.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(responseBody))
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseBody));
     }
