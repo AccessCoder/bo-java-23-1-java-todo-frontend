46149f927500fcbaf212a273a55c498ff661e517
Dominic <andro5@web.de>
2023-05-08 10:39:04 +0200

    add @service in RandomIdService

--- backend/src/main/java/de/neuefische/backend/service/RandomIdService.java
+++ backend/src/main/java/de/neuefische/backend/service/RandomIdService.java
@@ -1,7 +1,10 @@
 package de.neuefische.backend.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
+@Service
 public class RandomIdService {
 
     public String generateRandomId(){
